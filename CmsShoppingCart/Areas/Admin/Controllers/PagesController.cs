341eff2df71d026b49f5c69faf6c078f71ae5e9f
Ediezzle <edmore.mandikiyana@gmail.com>
2019-10-23 23:41:32 +0200

    Add functionality to sort pages and some custom css

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -32,9 +32,230 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
         }
 
         // GET: Admin/Pages/AddPage
+        [HttpGet]
         public ActionResult AddPage()
         {
             return View();
         }
+
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            //checked model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db= new Db())
+            {
+                //Declare slug
+                string slug;
+
+                //Initialise PageDTO
+                PageDTO dto = new PageDTO();
+
+                //DTO title
+                dto.Title = model.Title;
+
+                //Check for and set slug if need be
+                if(string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                //Make sure title and slug are unique
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "That title or slug already exists!");
+                    return View(model);
+                }
+
+                //DTO the rest
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSideBar = model.HasSideBar;
+                dto.Sorting = 100;
+
+                //Save DTO
+                db.Pages.Add(dto);
+                db.SaveChanges();
+
+                //Set TempData message
+                TempData["SuccessMessage"] = "You have added a new page!";
+
+                //Redirect
+                return RedirectToAction("AddPage");
+            }
+        }
+
+        // GET: Admin/Pages/EditPage/id
+        //if you dont specify Http method it is "Get" by Default
+        public ActionResult EditPage(int id)
+        {
+            //Declare PageVM
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+
+                //Get the page
+                PageDTO dto = db.Pages.Find(id);
+
+            //Confirm page exists
+            if (dto == null)
+                {
+                    return Content("The page does not exist!");
+                }
+
+                //Initialise PageVM
+                model = new PageVM(dto);
+
+                //Return view with model
+                return View(model);
+            }
+        }
+
+        // POST: Admin/Pages/EditPage/id
+        [HttpPost]
+        public ActionResult EditPage(PageVM model)
+        {
+            //check model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                //get page id
+                int id = model.Id;
+
+                //declare and initialise slug
+                string slug = "home";
+
+                //get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                //DTO title
+                dto.Title = model.Title;
+
+                //check for slug and set it if need be
+                if(model.Slug != "home")
+                {
+                    if(string.IsNullOrWhiteSpace(model.Slug))
+                    {
+                        slug = model.Title.Replace(" ", "-").ToLower();
+                    }
+
+                    else
+                    {
+                        slug = model.Slug.Replace(" ", "-").ToLower();
+                    }
+                }
+
+                //make sure the title and slug are unique
+                if(db.Pages.Where(x=>x.Id!=id).Any(x=>x.Title == model.Title) || db.Pages.Where(x => x.Id != id).Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "That title or slug already exists!");
+                    return View(model);
+                }
+
+                //DTO the rest
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSideBar = model.HasSideBar;
+
+                //save the DTO
+                db.SaveChanges();
+
+            }
+
+            //set the TempData message
+            TempData["SuccessMessage"] = "You have successfully edited the page!";
+
+            //redirect
+            return RedirectToAction("EditPage");
+            
+        }
+
+        // GET: Admin/Pages/PageDetails/id
+        public ActionResult PageDetails(int id)
+        {
+            //declare PageVM
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+
+                //get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                //confirm page exists
+                if(dto==null)
+                {
+                    return Content("The page doesn't exist!");
+                }
+
+                //init the PageVM
+                model = new PageVM(dto);
+
+            }
+            //return the view with model 
+            return View(model);
+        }
+
+        // GET: Admin/Pages/DeletePage/id
+        public ActionResult DeletePage(int id)
+        {
+            using (Db db = new Db())
+            {
+                //get page
+                PageDTO dto = db.Pages.Find(id);
+
+                //remove the page
+                db.Pages.Remove(dto);
+
+                //save changes
+                db.SaveChanges();
+                
+            }
+            return RedirectToAction("index");   
+        }
+
+        // POST: Admin/Pages/DeletePage/ReorderPages
+        [HttpPost]
+        public void ReorderPages(int[] id)
+        {
+            using(Db db = new Db())
+            {
+                //set initial count
+                int count = 1;
+
+                //declare PageDTO
+                PageDTO dto;
+
+                //set sorting for each page
+                foreach (var pageId in id)
+                {
+                    dto = db.Pages.Find(pageId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+
+                    count++;
+                }
+            }
+           
+        }
+
     }
+
+    
 }
